251015b40da72b62c5a111e1a90121ed16e145e3
karimkiller <karimadel66.ka@gmail.com>
2025-11-11 17:43:58 -0500

    feat(web): add missing pages, navbar, and interceptor

--- src/app/pages/timesheet/timesheet.component.ts
+++ src/app/pages/timesheet/timesheet.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core'
 import { FormsModule } from '@angular/forms'
 import { HttpClient } from '@angular/common/http'
 import { NgFor } from '@angular/common'
-import { environment } from '../../environments/environment'
+import { environment } from '../../../environments/environment'
 
 @Component({
   standalone:true, selector:'app-timesheet',
